80418535b7246505d979a16d038a3b038f615ab9
Jyotiraditya <jyotiraditya602@gmail.com>
2024-03-11 12:42:13 +0530

    modified:   package.json 	modified:   routes/products.route.js

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,7 +2,8 @@ const Product = require("./../models/product.model");
 
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    
+    const products = await Product.find(req.query);
     res.status(200).json(products);
   } catch (err) {
     res.status(500).json({ message: err.message });
